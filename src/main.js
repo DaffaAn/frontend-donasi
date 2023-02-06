25324e94670017f295f629aded35b14ed218fd96
DaffaAn <daffa19003@mail.unpad.ac.id>
2023-02-06 20:59:26 +0700

    midtrans ongoing

--- src/main.js
+++ src/main.js
@@ -12,10 +12,18 @@ import "vue-toastification/dist/index.css";
 */
 import './index.css'
 
+/**
+ * Mixins
+ */
+import mixins from './mixins'
+
 //create App Vue
 const app = createApp(App)
 
 //gunakan "Toast" di Vue Js dengan plugin "use"
 app.use(Toast)
+
+//gunakan "Mixins" di Vue Js dengan plugin "use"
+app.mixin(mixins)
 
 app.mount('#app')
