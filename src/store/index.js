83be9b12da1860c02b615e28c60511e5d7e12e38
DaffaAn <daffa19003@mail.unpad.ac.id>
2023-02-11 19:17:44 +0700

    Register donatur done

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 //import vuex
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
 
 //import module auth
-import auth from "./module/auth";
+import auth from './module/auth'
 
 //create store vuex
 export default createStore({
-  modules: {
-    auth, // <-- module auth
-  },
-});
+
+    modules: {
+        auth,       // <-- module auth
+    }
+
+})
