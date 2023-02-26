3b718e57e47ac7d08f9f63706be1811d08fb6196
DaffaAn <daffa19003@mail.unpad.ac.id>
2023-02-26 09:32:35 +0700

    donasi saya done

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 //import vuex
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 //import module auth
-import auth from './module/auth'
+import auth from "./module/auth";
+
+//import module donation
+import donation from "./module/donation";
 
 //create store vuex
 export default createStore({
-
-    modules: {
-        auth,       // <-- module auth
-    }
-
-})
+  modules: {
+    auth, // <-- module auth
+    donation, // <-- module donation
+  },
+});
