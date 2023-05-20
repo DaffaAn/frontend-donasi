e95b7987e64174c1c89abf1844a310836d0d727c
DaffaAn <daffa19003@mail.unpad.ac.id>
2023-05-20 21:50:06 +0700

    about done

--- src/views/about/Index.vue
+++ src/views/about/Index.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="pb-20 pt-20 text-center">HALAMAN ABOUT</div>
+  <div class="mt-5 grid gap-4 ml-10 mr-10">
+    <div class="bg-white rounded-md shadow-md p-2">
+      <div class="md:flex rounded md:p-0">
+        <p class="text-justify">
+          Masjid Jenderal Sudirman (MJS) Jakarta merupakan masjid yang berada di dalam komplek
+          perkantoran World Trade Center (WTC) di jalan jenderal Sudirman Jakarta. Masjid ini
+          memiliki visi sebagai pusat pembinaan dan pengembangan ummat, serta menjadi rujukan masjid
+          perkantoran dalam mmembangun peradaban islami. Sebagai masjid perkantoran, MJS memiliki
+          beberapa program untuk para jamaah seperti sholat berjamaah, kajian dzuhur perkantoran,
+          belajar cara baca alqur’an yang baik dan benar dan program LAZIS MJS. <br />LAZIS Masjid
+          Jenderal Sudirman merupakan unit kegiatan yang berada di bawah manajemen Masjid Jenderal
+          Sudirman Jakarta. Adapun keberadaan unit ini diperlukan sebagai wadah untuk mengumpulkan
+          dana zakat, infaq dan Sedekah dari para jamaah dan donatur pada umumnya. Dana yang
+          terkumpul disalurkan kepada anak yatim, dhufa dan fakir miskin dalam berbagai program
+          seperti santunan anak yatim, belanja bareng yatim, beasiswa yatim, santunan sembako dan
+          modal usaha dhufa dan fakir miskin.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
