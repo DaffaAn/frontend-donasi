4f30c720bd7f701ae643ffa609d3b33803b6bcf3
DaffaAn <daffa19003@mail.unpad.ac.id>
2023-05-27 17:48:18 +0700

    logo lazis

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,9 +4,9 @@
     <header>
       <div class="bg-gray-700 text-white text-center fixed inset-x-0 top-0 z-10">
         <div class="container mx-auto grid grid-cols-10 p-3 sm:w-full md:w-5/12">
-          <div class="col-span-2 bg-white rounded-full h-10 w-10 p-1 mr-3 shadow-sm">
+          <div class="col-span-2 bg-white rounded-full h-10 w-30 p-1 mr-3 shadow-sm">
             <router-link :to="{ name: 'home' }">
-              <img src="@/assets/images/muslim.png" class="inline-block" />
+              <img src="@/assets/images/lazis.png" class="inline-block" />
             </router-link>
           </div>
           <div class="col-span-8">
